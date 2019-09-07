ab15fd0b432c5626c73bfaf26827135dd64b6c3a
Jaxson <i@iiong.com>
2019-09-08 00:33:59 +0800

    完善功能！

--- assets/main/main.js
+++ assets/main/main.js
@@ -2,6 +2,7 @@ var ghostSearchkey = '99efee9603c92e5cd04501f069'; // 搜索key
 var baiduTongJikey = '37d3bf3116f041cb10bd1d890e65bcfc'; // 百度统计id
 var leancloudAppId = 'rEDT0uBB2LEdndoJ4od2SlKf-gzGzoHsz'; // leancloud 应用 appid
 var leancloudAppKey = 'lmX57j7hrYGCHROA72tBUIXq';  // leancloud 应用 appkey
+var googleAnalyticsId = 'UA-141063659-1'; // 谷歌分析ID
 
 // key: '1c8b902ac09889962117d082e6',
 // key: '99efee9603c92e5cd04501f069',
@@ -9,22 +10,21 @@ var leancloudAppKey = 'lmX57j7hrYGCHROA72tBUIXq';  // leancloud 应用 appkey
 (function ($) {
   $(document).ready(function () {
     //百度推送
-    var bp = document.createElement('script');
     var curProtocol = window.location.protocol.split(':')[0];
     if (curProtocol === 'https') {
-      bp.src = 'https://zz.bdstatic.com/linksubmit/push.js';
+      loadScript('https://zz.bdstatic.com/linksubmit/push.js');
     } else {
-      bp.src = 'http://push.zhanzhang.baidu.com/push.js';
+      loadScript('http://push.zhanzhang.baidu.com/push.js');
     }
-    var bdPush = document.getElementsByTagName('script')[0];
-    bdPush.parentNode.insertBefore(bp, bdPush);
-
     //百度统计
-    var hm = document.createElement('script');
-    hm.src = 'https://hm.baidu.com/hm.js?' + baiduTongJikey;
-    var bdhmt = document.getElementsByTagName('script')[0];
-    bdhmt.parentNode.insertBefore(hm, bdhmt);
-
+    loadScript('https://hm.baidu.com/hm.js?' + baiduTongJikey);
+    // 谷歌分析
+    loadScript('https://www.googletagmanager.com/gtag/js?id=' + googleAnalyticsId, function () {
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', googleAnalyticsId);
+    });
     //valine评论支持
     if($('#vcomments').length !== 0) {
       loadScript('//cdn.jsdelivr.net/npm/leancloud-storage/dist/av-min.js', function () {
@@ -49,7 +49,6 @@ var leancloudAppKey = 'lmX57j7hrYGCHROA72tBUIXq';  // leancloud 应用 appkey
         );
       });
     }
-
     // 配置搜索
     if($('#ghost-search-field').length !== 0) {
       loadScript('//cdn.jsdelivr.net/npm/@tryghost/content-api/umd/content-api.min.js', function () {
@@ -114,7 +113,6 @@ var leancloudAppKey = 'lmX57j7hrYGCHROA72tBUIXq';  // leancloud 应用 appkey
         });
       });
     }
-
     // 监听点击链接时间，非本站链接进行新标签打开
     $(document).on('click', 'a', function (event) {
       var link = event.target.href; // 完整链接
@@ -124,16 +122,6 @@ var leancloudAppKey = 'lmX57j7hrYGCHROA72tBUIXq';  // leancloud 应用 appkey
           event.preventDefault();
           window.open(event.target.href)
         }
-      }
-    });
-
-    // Google 广告配置
-    loadScript('https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js', function () {
-      (adsbygoogle = window.adsbygoogle || []).push({});
-      console.log('Google广告加载完毕。嘤嘤嘤，别把本站广告屏蔽了哟！');
-      if (adsbygoogle.loaded || window.adsbygoogle.loaded) {
-        // 当谷歌广告没有被屏蔽的时候 - 删除提示语
-        $('.shielding-ads-tips').remove();
       }
     });
   });
